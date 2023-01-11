377441a785a9b83a22a157202a4b9c4183c578a2
Lahiru Maramba <llahiru@gmail.com>
2023-01-11 17:27:38 -0500

    changed loggers

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,7 +1,11 @@
 package com.example
 
+import org.slf4j.bridge.SLF4JBridgeHandler
+
 import io.micronaut.runtime.Micronaut.run
 fun main(args: Array<String>) {
+	SLF4JBridgeHandler.removeHandlersForRootLogger()
+    SLF4JBridgeHandler.install()
 	run(*args)
 }
 
